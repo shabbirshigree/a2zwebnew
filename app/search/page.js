304ac22efd53bda1b46531a45bc3201c6f3698f0
shabbirshigree <shigri51214@gmail.com>
2026-03-16 06:23:36 +0500

    fixing errors and updating gallery

---
+++ app/search/page.js
@@ -0,0 +1,10 @@
+// app/search/page.js
+
+export default function SearchPage() { // 🔴 'default' کا ہونا لازمی ہے
+  return (
+    <div>
+      {/* آپ کا سرچ پیج کا کوڈ یہاں ہوگا */}
+      <h1>سرچ کے نتائج</h1>
+    </div>
+  );
+}
