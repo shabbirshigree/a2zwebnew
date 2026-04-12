7815d9ec7e58d9e109b9aace5e48bd37aba35805
shabbirshigree <shigri51214@gmail.com>
2026-04-12 16:04:00 +0500

    Fixing case sensitivity and paths

--- app/noor-ul-quran/page.js
+++ app/noor-ul-quran/page.js
@@ -7,7 +7,7 @@ import Link from 'next/link';
 import { Navbar, HeroSlider } from '../components/Header';
 import Footer from '../components/Footer';
 import QuranIntroCard from '../components/QuranIntroCard';
-import { quranVideos } from './projectData';
+import { quranVideos } from '../noor-ul-quran/projectData';
 export default function ProjectPage() {
   const router = useRouter();
   const [selectedVideo, setSelectedVideo] = useState(null);
