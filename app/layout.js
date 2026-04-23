260d4a813c8c734e2f5d62e71f536d2f9b67d5a4
shabbirshigree <shigri51214@gmail.com>
2026-04-24 04:59:53 +0500

    fixing errors and updating gallery

--- app/layout.js
+++ app/layout.js
@@ -48,7 +48,7 @@ const kufi = Reem_Kufi({
 // ویب سائٹ کی مکمل ایس ای او (SEO) سیٹنگ
 export const metadata = {
   title: 'حاجی شبیر احمد شگری | آفیشل ویب سائٹ',
-  description: 'حاجی شبیر احمد شگری کی علمی، تحقیقی اور صحافتی خدمات کا مجموعہ۔ نور القرآن پروجیکٹ اور اتحادِ امت کے لیے خصوصی کاوشیں۔',
+  description: 'دنیا کے پہلے ویژول(بصری)قرآن پراجیکٹ اور اس کے بانی حاجی شبیر احمد شگری کی 45 سالہ خدمات کے بارے میں جانئے',
   keywords: 'Shabbir Ahmed Shigri, Noor-ul-Quran, Visual Quran, Islamic Scholar, Journalist, Pakistan, Iran, Unity of Ummah',
   icons: {
     icon: 'https://res.cloudinary.com/dtqrziupt/image/upload/v1772106162/fe64b922-ae4d-4243-b541-9849b90c34df.png',
@@ -60,21 +60,21 @@ export const metadata = {
   },
   openGraph: {
     title: 'نورالقرآن پراجیکٹ | حاجی شبیر احمد شگری',
-    description: 'دنیا کا پہلا ویژول قرآن پراجیکٹ اور حاجی شبیر احمد شگری کی 45 سالہ خدمات۔',
+    description: 'دنیا کے پہلے ویژول(بصری)قرآن پراجیکٹ اور اس کے بانی حاجی شبیر احمد شگری کی 45 سالہ خدمات کے بارے میں جانئے',
     url: 'https://www.shigri.info',
     siteName: 'Haji Shabbir Ahmed Shigri',
     images: [
-      {
-        url: 'https://res.cloudinary.com/dtqrziupt/image/upload/v1772106162/fe64b922-ae4d-4243-b541-9849b90c34df.png',
-        width: 800,
-        height: 800,
-        alt: 'Noor-ul-Quran Logo',
-      },
       {
         url: 'https://res.cloudinary.com/dtqrziupt/image/upload/v1774145249/noorulquran-proj-cover_bhvb0d.png',
         width: 1200,
         height: 630,
         alt: 'Noor-ul-Quran Project Cover',
+      },
+      {
+        url: 'https://res.cloudinary.com/dtqrziupt/image/upload/v1772106162/fe64b922-ae4d-4243-b541-9849b90c34df.png',
+        width: 800,
+        height: 800,
+        alt: 'Noor-ul-Quran Logo',
       },
     ],
     locale: 'ur_PK',
@@ -83,12 +83,12 @@ export const metadata = {
   twitter: {
     card: 'summary_large_image',
     title: 'نورالقرآن پراجیکٹ | حاجی شبیر احمد شگری',
-    description: 'دنیا کا پہلا ویژول قرآن پراجیکٹ اور حاجی شبیر احمد شگری کی 45 سالہ خدمات۔',
-    images: ['https://res.cloudinary.com/dtqrziupt/image/upload/v1772106162/fe64b922-ae4d-4243-b541-9849b90c34df.png'],
+    description: 'دنیا کے پہلے ویژول(بصری)قرآن پراجیکٹ اور اس کے بانی حاجی شبیر احمد شگری کی 45 سالہ خدمات کے بارے میں جانئے',
+    images: ['https://res.cloudinary.com/dtqrziupt/image/upload/v1774145249/noorulquran-proj-cover_bhvb0d.png'],
   },
   whatsapp: {
     title: 'نورالقرآن پراجیکٹ | آفیشل ویب سائٹ',
-    description: 'حاجی شبیر احمد شگری کی علمی و دینی خدمات اور دنیا کا پہلا ویژول قرآن۔',
+    description: 'دنیا کے پہلے ویژول(بصری)قرآن پراجیکٹ اور اس کے بانی حاجی شبیر احمد شگری کی 45 سالہ خدمات کے بارے میں جانئے',
     status: 'online',
   }
 }
--- app/noor-ul-quran/layout.js
+++ app/noor-ul-quran/layout.js
@@ -1,25 +1,25 @@
 export const metadata = {
   title: 'نورالقرآن پراجیکٹ | دنیا کا پہلا ویژول قرآن',
-  description: 'حاجی شبیر احمد شگری کا منفرد منصوبہ: جدید ٹیکنالوجی اور اے آئی کے ذریعے قرآن مجید کا بصری ترجمہ و تفہیم۔',
+  description: 'دنیا کے پہلے ویژول(بصری)قرآن پراجیکٹ اور اس کے بانی حاجی شبیر احمد شگری کی 45 سالہ خدمات کے بارے میں جانئے',
   icons: {
     icon: 'https://res.cloudinary.com/dtqrziupt/image/upload/v1772106162/fe64b922-ae4d-4243-b541-9849b90c34df.png',
   },
   openGraph: {
     title: 'نورالقرآن پراجیکٹ (The Visual Quran)',
-    description: 'قرآن مجید کو سمجھ کر پڑھنے اور عمل کرنے کے لیے ایک جدید بصری کاوش۔',
+    description: 'دنیا کے پہلے ویژول(بصری)قرآن پراجیکٹ اور اس کے بانی حاجی شبیر احمد شگری کی 45 سالہ خدمات کے بارے میں جانئے',
     url: 'https://www.shigri.info/noor-ul-quran',
     images: [
+      {
+        url: 'https://res.cloudinary.com/dtqrziupt/image/upload/v1774145249/noorulquran-proj-cover_bhvb0d.png',
+        width: 1200,
+        height: 630,
+        alt: 'Noor-ul-Quran Visual Project Cover',
+      },
       {
         url: 'https://res.cloudinary.com/dtqrziupt/image/upload/v1772106162/fe64b922-ae4d-4243-b541-9849b90c34df.png',
         width: 800,
         height: 800,
         alt: 'Noor-ul-Quran Logo',
-      },
-      {
-        url: 'https://res.cloudinary.com/dtqrziupt/image/upload/v1774145249/noorulquran-proj-cover_bhvb0d.png',
-        width: 1200,
-        height: 630,
-        alt: 'Noor-ul-Quran Visual Project',
       },
     ],
     locale: 'ur_PK',
@@ -28,8 +28,8 @@ export const metadata = {
   twitter: {
     card: 'summary_large_image',
     title: 'نورالقرآن پراجیکٹ | The Visual Quran',
-    description: 'دنیا کا پہلا ویژول قرآن پراجیکٹ - حاجی شبیر احمد شگری کی پیشکش۔',
-    images: ['https://res.cloudinary.com/dtqrziupt/image/upload/v1772106162/fe64b922-ae4d-4243-b541-9849b90c34df.png'],
+    description: 'دنیا کے پہلے ویژول(بصری)قرآن پراجیکٹ اور اس کے بانی حاجی شبیر احمد شگری کی 45 سالہ خدمات کے بارے میں جانئے',
+    images: ['https://res.cloudinary.com/dtqrziupt/image/upload/v1774145249/noorulquran-proj-cover_bhvb0d.png'],
   },
 }
 
